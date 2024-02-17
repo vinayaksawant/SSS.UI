f2acb17880f218e736f6a770eefabc0c83877ce8
vinayak.sawant <vinayaksawant@hotmail.com>
2024-02-16 20:48:13 -0600

    added Image service

--- src/app/Shared/services/image-upload.service.ts
+++ src/app/Shared/services/image-upload.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { FileUpload } from '../models/fiile-upload.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageUploadService {
+  selectedImage: BehaviorSubject<FileUpload> = new BehaviorSubject<FileUpload>({
+    id: '',
+    fileExtenstion: '',
+    fileName: '',
+    title: '',
+    url: ''
+  });
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllFileUploads(): Observable<FileUpload[]> {
+    return this.http.get<FileUpload[]>(`${environment.apiBaseUrl}/api/ImageFiles`);
+  }
+
+
+  PostFileUpload(file: File, fileName: string, title: string): Observable<FileUpload> {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', fileName);
+    formData.append('title', title);
+    formData.append('fileType', "JobPosting");    
+
+    return this.http.post<FileUpload>(`${environment.apiBaseUrl}/api/ImageFiles`, formData);
+  }
+
+  selectImage(image: FileUpload): void {
+    this.selectedImage.next(image);
+  }
+
+  onSelectImage(): Observable<FileUpload> {
+    return this.selectedImage.asObservable()
+  }
+
 }
